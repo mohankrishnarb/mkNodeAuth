ae7db46b44fc5bd83a7e2e1abe06ac2179ccdc67
Ujawal Tiwari <ujawal0011@gmail.com>
2022-12-04 18:14:04 +0530

    Start

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3005;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+});
